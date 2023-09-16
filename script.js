089d46adacd253e82d42428fe52feda40eb85987
Rohit Kumar <126482082+rsingh241020@users.noreply.github.com>
2023-09-16 16:37:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
-    //Write your code here
+    event.preventDefault(); // Prevent the form from submitting
 
+    // Get the values of first name and last name from the form
+    const firstName = document.querySelector('input[name="fname"]').value;
+    const lastName = document.querySelector('input[name="lname"]').value;
+
+    // Check if both first name and last name are provided
+    if (firstName && lastName) {
+        alert(`First Name: ${firstName}\nLast Name: ${lastName}`);
+    } else {
+        alert('Please provide both First Name and Last Name.');
+    }
 }
